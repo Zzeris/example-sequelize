753f7e9e0e4490bb7bddbfbdf90e6c21b557f340
zzeris <zzeris@gmail.com>
2019-11-01 18:30:47 -0300

    Add techs and association

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router()
 const UserController = require('./controllers/UserController')
 const AddressController = require('./controllers/AddressController')
+const TechController = require('./controllers/TechController')
 
 router.post('/users', UserController.store)
 router.get('/users', UserController.index)
 
-router.post('/users/:user_id/addresses', AddressController.index)
+router.get('/users/:user_id/addresses', AddressController.index)
 router.post('/users/:user_id/addresses', AddressController.store)
+
+router.get('/users/:user_id/techs', TechController.index)
+router.post('/users/:user_id/techs', TechController.store)
+router.delete('/users/:user_id/techs', TechController.delete)
 
 module.exports = router
